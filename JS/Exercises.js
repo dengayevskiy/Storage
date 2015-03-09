da6c3786606300c205dbd4334b45b311a808cfa2
Denis Gayevskiy <den.gayevskiy@gmail.com>
2015-03-09 12:00:52 +0200

    add js

---
+++ JS/Exercises.js
@@ -0,0 +1,23 @@
+//2.1 Looping a triangle
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+//2.2 FizzBuzz(1)
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+//2.2 FizzBuzz(2)
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0)
+    console.log('FizzBuzz');
+  if (i % 3 === 0)
+    console.log('Fizz');
+  if (i % 5 === 0)
+    console.log('Buzz');
+  else(console.log(i));
+}
---
+++ JS/tutorial.js
@@ -0,0 +1,13 @@
+
+var n = 144;
+var answer = Math.sqrt(n);
+console.log(answer);
+
+function validator() {
+  if (document.myForm.name.checked) {
+    alert("It's checked!")
+  }
+  else {
+    alert("No, it's not checked!")
+  }
+}
