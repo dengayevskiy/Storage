c6f9718a4c8726d1d6bc7b3c4c12e16a83fdedd6
dengayevskiy <den.gayevskiy@gmail.com>
2015-03-16 18:23:21 +0200

    123

--- JS/Exercises.js
+++ JS/Exercises.js
@@ -11,6 +11,7 @@ for (var n = 1; n <= 100; n++) {
     output += "Buzz";
   console.log(output || n);
 }
+
 //2.2 FizzBuzz(2)
 for (var i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0)
@@ -21,4 +22,39 @@ for (var i = 1; i <= 100; i++) {
     console.log('Buzz');
   else(console.log(i));
 }
+
 //2.3 FizzBuzz(2)
+
+//3.1 Min
+function min(x, y) {
+  if (x < y)
+  	return x
+  else
+    return y
+}
+
+//3.2 isEven
+function isEven(number) {
+  	if (number == 0)
+      return true;
+    else if (number == 1)
+      return false;
+  	else if (number < 0)
+      return isEven(-number)
+  	else
+      return isEven(number - 2);
+}
+
+//3.3 Bean counting
+function countBs(phrase) { //считает буквы 'B'
+	return countChar(phrase, 'B');
+}
+
+function countChar(phrase, char) { //считает те, что вввел(char)
+	var number = 0;
+  for (var i = 0; i < phrase.length; i++) {
+  	if (phrase.charAt(i) === char)
+      number += 1;
+  }
+  return number;
+}
